3092f67cc038cbf8b16113ead4924ceba3e4bf74
olyv <a.n.lyvin@gmail.com>
2014-10-30 22:52:30 +0200

    added page object related SighUpPage class

---
+++ src/main/java/com/mycompany/openforbeta/pages/SignUpPage.java
@@ -0,0 +1,14 @@
+package com.mycompany.openforbeta.pages;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Describes signup page
+ */
+public class SignUpPage extends BasePage
+{
+    public SignUpPage(WebDriver driver)
+    {
+        super(driver);
+    }
+}
